bb9e09281436118d6351ede2517d3f78ec0f97fe
hungmuha <hung.soboss@gmail.com>
2018-02-17 15:15:32 -0700

    learning about state full component

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ npm run server
 ````
 
 The server is now running at [localhost:3000](localhost:3000)
+
+law to determine stateful component
+1. Is it passed in from a parent via props? If so, it probably isn’t state.
+	A lot of the data used in our child components are already listed in their parents. This criterion helps us de-duplicate.
+	For example, “timer properties” is listed multiple times. When we see the properties declared in EditableTimerList, we can consider it state. But when we see it elsewhere, it’s not.
+2. Does it change over time? If not, it probably isn’t state.
+	This is a key criterion of stateful data: it changes.
+3. Can you compute it based on any other state or props in your component? If so, it’s not state.
+	For simplicity, we want to strive to represent state with as few data points as possible.
--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ class TimersDashboard extends React.Component{
         		<div className='column'>
           			<EditableTimerList />
          			<ToggleableTimerForm
-						isOpen={true} 
+						isOpen={false} 
 					/>
         		</div>
       		</div>
@@ -89,6 +89,7 @@ class TimerForm extends React.Component {
 	}
 }
 
+//component that display the plus sight or form to add more timing components
 class ToggleableTimerForm extends React.Component {
 	render(){
 		if(this.props.isOpen){
@@ -106,3 +107,40 @@ class ToggleableTimerForm extends React.Component {
 		}
 	}
 }
+
+class Timer extends React.Component {
+	render(){
+		const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+		return(
+			<div className='ui centered card'>
+        		<div className='content'>
+					<div className='header'> 
+						{this.props.title}
+					</div>
+					<div className='meta'> 
+						{this.props.project}
+          			</div>
+         		 	<div className='center aligned description'>
+            			<h2>{elapsedString}</h2>				
+					</div>
+					<div className='extra content'>
+						<span className='right floated edit icon'>
+						  <i className='edit icon' />
+						</span>
+						<span className='right floated trash icon'>
+						  <i className='trash icon' />
+						</span>
+					</div>
+				</div>
+        		<div className='ui bottom attached blue basic button'>
+					Start
+        		</div>
+     		</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<TimersDashboard/>,
+	document.getElementById('content')
+);
